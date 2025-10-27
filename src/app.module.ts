3cb793d629d3acf5c0b85b7c318b6c39a4278e6f
Jalal Taratingan <jalaltaratingan14@gmail.com>
2025-10-27 21:58:06 +0800

    officer and student controllers/services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentController } from './student/student.controller';
+import { StudentService } from './student/student.service';
+import { OfficerController } from './officer/officer.controller';
+import { OfficerService } from './officer/officer.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, StudentController, OfficerController],
+  providers: [AppService, StudentService, OfficerService],
 })
 export class AppModule {}
