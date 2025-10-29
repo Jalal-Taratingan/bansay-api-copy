83b6322f9ca726dfd963cde12726af4a9fd517f8
Jalal Taratingan <jalaltaratingan14@gmail.com>
2025-10-29 12:20:24 +0800

    nestJS configuration setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,30 @@ import { LiabilityModule } from './liability/liability.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule} from '@nestjs/typeorm';
 import { Student } from './user/entities/student.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5433,
-    username: 'user',
-    password: 'password',
-    database: 'bansay_db',
-    entities: [Student],
-    synchronize: true,
+  imports: [ 
+  ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: ['.env'],
+    load: [configuration]
+  }), 
+  TypeOrmModule.forRootAsync({
+    useFactory: (configService: ConfigService) => {
+      return {
+        type: 'postgres',
+        host: configService.get('database.host') || 'localhost',
+        port: configService.get('database.port') || 5433,
+        username: configService.get('database.username') || 'user',
+        password: configService.get('database.password') || 'password',
+        database: configService.get('database.dbName') || 'bansay_db',
+        entities: [Student],
+        synchronize: true,
+    }
+    },
+    inject: [ConfigService]
   }),
     LiabilityModule, UserModule],
   controllers: [AppController],
---
+++ src/config/configuration.ts
@@ -0,0 +1,12 @@
+
+export default () => ({
+    port: parseInt(String(process.env.PORT), 10) || 3030,
+    database: {
+      host: process.env.DATABASE_HOST,
+      port: parseInt(String(process.env.DATABASE_PORT), 10) || 5433,
+      dbName: process.env.DATABASE_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    }
+  });
+  
